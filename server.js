a6f36affca1819ca85b114f52d79736574dbfa65
Diego <diegodiaz0717@gmail.com>
2026-02-08 22:25:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import app from "./src/app.js";
+const app = require('./src/app');
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-Create server file
